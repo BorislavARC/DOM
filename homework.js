be65d13d08039f96524ccd2915ca59ed64483f3b
boris <Borislav.arc@gmail.com>
2014-01-27 18:45:22 +0200

    perviy commit

--- homework.js
+++ homework.js
@@ -1,22 +1,49 @@
-/**
- * Обработчик клика по ссылке с классом 'popup-link'
- * @param {Event} e событие клика
- * @private
- */
-function _onMouseClick(e) {}
 
-/**
- * Получает данные из ссылки
- * на основе этих данных создаёт попап (через createPopup) и добавляет его в DOM
- * @param {HTMLElement} link Ссылка с data-аттрибутами
- */
-function openPopupFromLink(link) {}
+var links = document.getElementsByClassName('popup-link'),
+    popupWrap = document.createElement('div'),
+    link, href, onOk, title, message, i;
 
-/**
- * Создаёт DOM-узел с сообщением
- * @param {String} title Заголовок сообщение
- * @param {String} message Текст сообщения сообщение
- * @param {Function} onOk Обработчик клика по кнопке 'Да'
- * @returns {HTMLElement}
- */
-function createPopup(title, message, onOk) {}
+for(i = 0; i < links.length; i++) {
+    links[i].onclick = _onMouseClick;
+}
+
+
+function _onMouseClick(e) {
+    e.preventDefault ? e.preventDefault() : (e.returnValue=false);
+    link = this;
+
+    return openPopupFromLink(link);
+}
+
+
+function openPopupFromLink(link) {
+    href = link.getAttribute('href');
+    console.log(href);
+    title = link.dataset.title;
+    console.log(title);
+    message = (link.dataset.message).replace(/%s/, href);
+    onOk = href;
+
+    return createPopup(title, message, onOk)
+}
+
+
+function createPopup(title, message, onOk) {
+    popupWrap.innerHTML = '<div class="popup-wrap"> \
+    <div class="popup"> \
+    <i class="close" onclick="closePopup()"></i>\
+    <h3>' + title + '</h3> \
+    <div class="content">' + message + '</div> \
+    <button id="no" value="no" onclick="closePopup()"> Нет </button> \
+    <button id="ok" value="ok" onclick="window.location = onOk"> Да </button> \
+  </div></div>';
+
+    return document.body.appendChild(popupWrap);
+}
+
+
+function closePopup() {
+    document.body.removeChild(popupWrap)
+}
+
+

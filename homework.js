a27eae17e726684b4d0d3851326d1910704774b3
boris <Borislav.arc@gmail.com>
2014-01-28 21:21:19 +0200

    popravki

--- homework.js
+++ homework.js
@@ -1,24 +1,27 @@
 
-var links = document.getElementsByClassName('popup-link'),
-    popupWrap = document.createElement('div'),
-    link, onOk, title, message, href;
-
-links[0].addEventListener("click", _onMouseClick);
-links[1].addEventListener("click", _onMouseClick);
+var popupWrap = document.createElement('div'),
+    onOk;
+document.getElementsByClassName('content')[0].onclick = _onMouseClick;
 
 
 function _onMouseClick(e) {
-    e.preventDefault ? e.preventDefault() : (e.returnValue=false);
-    link = this;
+    var event = e || window.event;
+    var target = event.target || event.srcElement;
 
-    return openPopupFromLink(link);
+    if (target.tagName != 'A') {
+        return false;
+    } else {
+        event.preventDefault ? event.preventDefault() : (event.returnValue=false);
+
+    return openPopupFromLink(target);
+    }
 }
 
 
 function openPopupFromLink(link) {
-    href = link.getAttribute('href');
-    title = link.dataset.title;
-    message = link.dataset.message.replace(/%s/, href);
+    var href = link.getAttribute('href');
+    var title = link.dataset.title;
+    var message = link.dataset.message.replace(/%s/, href);
     onOk = function() {
         window.location = href;
     };
@@ -32,7 +35,7 @@ function createPopup(title, message, onOk) {
     popupWrap.innerHTML = '<div class="popup"> \
     <i class="close" onclick="document.body.removeChild(popupWrap)"></i>\
     <h3>' + title + '</h3> \
-    <div class="content">' + message + '</div> \
+    <div class="popup-content">' + message + '</div> \
     <input type="button" value="Нет" onclick="document.body.removeChild(popupWrap)"> \
     <input type="button" value="Да" onclick="onOk()"> \
     </div>';

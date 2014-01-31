2f16c655b3880bd913a14c414c7f5ce0fff4adb8
boris <Borislav.arc@gmail.com>
2014-01-31 21:50:19 +0200

    popravil bagi

--- homework.js
+++ homework.js
@@ -6,50 +6,50 @@ function _onMouseClick(e) {
     var event = e || window.event;
     var target = event.target || event.srcElement;
 
-    if (!(target.classList.contains('popup-link'))) {
-        return false;
-    } else {
+    if (target.classList.contains('popup-link')) {
         event.preventDefault ? event.preventDefault() : (event.returnValue=false);
-
-        return openPopupFromLink(target);
+        openPopupFromLink(target);
     }
 }
 
 
 function openPopupFromLink(link) {
-    var href = link.getAttribute('href');
-    var title = link.dataset.title;
-    var message = link.dataset.message.replace(/%s/, href);
-    var onOk = function() {
-        window.location = href;
-    };
+    var title = link.dataset.title,
+        message = link.dataset.message.replace(/%s/, link.getAttribute('href'));
 
-    return createPopup(title, message, onOk)
-}
-
-
-function createPopup(title, message, onOk) {
-    var elem = document.getElementsByClassName('popup-wrap')[0];
-    if(elem !== undefined) {
-        elem.style.display = 'block';
+    if(document.getElementsByClassName('popup-wrap')[0] !== undefined) {
+        document.getElementsByClassName('popup-title')[0].innerHTML = title;
+        document.getElementsByClassName('popup-content')[0].innerHTML = message;
+        document.getElementsByClassName('popup-wrap')[0].style.display = 'block';
     } else {
-    var popupWrap = document.createElement('div');
-    popupWrap.className = 'popup-wrap';
-    popupWrap.innerHTML = '<div class="popup"> \
-    <i class="close"></i>\
-    <h3>' + title + '</h3> \
-    <div class="popup-content">' + message + '</div> \
-    <input class="not" type="button" value="Нет"> \
-    <input class="yes" type="button" value="Да"> \
-    </div>';
-
-    document.body.appendChild(popupWrap);
+        createPopup(
+            title,
+            message,
+            function() {
+                window.location = link.getAttribute('href');
+                document.getElementsByClassName('popup-wrap')[0].style.display = 'none';})
     }
 
-    function del() {
-       document.getElementsByClassName('popup-wrap')[0].style.display = 'none';
+
+
+    function createPopup(title, message, onOk) {
+        var popupWrap = document.createElement('div');
+        popupWrap.className = 'popup-wrap';
+        popupWrap.innerHTML = '<div class="popup"> \
+            <i class="close"></i>\
+            <h3 class="popup-title">' + title + '</h3> \
+            <div class="popup-content">' + message + '</div> \
+            <input class="not" type="button" value="Нет"> \
+            <input class="yes" type="button" value="Да"> \
+            </div>';
+
+        document.body.appendChild(popupWrap);
+
+        function del() {
+            document.getElementsByClassName('popup-wrap')[0].style.display = 'none';
+        }
+        document.getElementsByClassName('close')[0].addEventListener('click', del);
+        document.getElementsByClassName('not')[0].addEventListener('click', del);
+        document.getElementsByClassName('yes')[0].addEventListener('click', onOk);
     }
-    document.getElementsByClassName('close')[0].addEventListener('click', del);
-    document.getElementsByClassName('not')[0].addEventListener('click', del);
-    document.getElementsByClassName('yes')[0].addEventListener('click', onOk);
 }
